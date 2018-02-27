46214679ccd62d194b30bf674f812b4c21970705
H. Thomson Comer <thomcom@gmail.com>
2018-02-26 17:15:57 -0700

    Add requirements.txt for auto build on travis.
    first three tests passing
    
    added derivative fxs, not sure if they are correct
    
    added vector fx sum and less, sum fx returns  failure when testing, less passes with no failure
    
    sum and less fxs are correct I think
    
    assignment complete
    
    tests passing, assignment complete

--- precourse.py
+++ precourse.py
@@ -11,3 +11,27 @@
 def f(x):
     return x**2
 
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return (x**3) + (5*x)
+
+def d_f(x):
+    return 2*x
+
+def d_f_2(x):
+    return 3*(x**2)
+
+def d_f_3(x):
+    return 3*(x**2) + (5*x)
+
+def vector_sum(x, y):
+    # for all values of x, return x + y
+    for num in range(len(x)):
+        return [x[num] + y[num]]
+
+def vector_less(x, y):
+    #  for all values of x, return x - y
+    for num in range(len(x)):
+        return [x[num] - y[num]]
