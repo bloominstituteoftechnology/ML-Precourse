129d0e208ece0b69c6ca744b056f1878af38a5c8
axrd <alejandroxrd@gmail.com>
2018-03-14 04:20:14 -0400

    Completed precourse.py

--- precourse.py
+++ precourse.py
@@ -11,3 +11,53 @@
 def f(x):
     return x**2
 
+import numpy as np
+
+## Calculus
+
+# Remaining 2 of 3 functions, defined.
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return x**3 + 5*x
+
+# Derivatives of said functions
+def d_f(x):
+    return 2*x
+
+def d_f_2(x):
+    return 3*x**2
+
+def d_f_3(x):
+    return 3*x**2 + 5
+
+## Linear Algebra
+
+# Vectors
+
+def vector_sum(x,y):
+    return np.add(x,y)
+
+def vector_less(x,y):
+    return np.subtract(x,y)
+
+def vector_magnitude(x):
+    return np.linalg.norm(x)
+
+def vec5():
+    return np.ones(5)
+
+def vec3():
+    return np.zeros(3)
+
+def vec2_1():
+    return np.array([1,0])
+
+def vec2_2():
+    return np.array([0,1])
+
+# Matrix
+
+def matrix_multiply(v,m):
+        return np.dot(v,m)
