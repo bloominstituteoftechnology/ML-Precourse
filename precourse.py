8b9a0432c258080b88e302e5719a4b4581b445eb
JC Soriano <jc@juancarlosoriano.ca>
2018-03-14 07:51:53 -0400

    Initial commit

--- precourse.py
+++ precourse.py
@@ -7,7 +7,8 @@
 # ###
 
 # Free example function definition
-# This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
+# This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. 
+# Passing this precourse work will greatly increase your odds of acceptance into the program.
 def f(x):
     return x**2
 
