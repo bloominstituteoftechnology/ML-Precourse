80505b775fa65227fb3f6e6c92aaa480bc8faa38
Samuel Cha <sunchipssam@gmail.com>
2018-03-15 15:09:45 -0700

    Finished calc portion

--- precourse.py
+++ precourse.py
@@ -8,6 +8,33 @@
 
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
+
+
 def f(x):
     return x**2
 
+
+def f_2(x):
+    return x**3
+
+
+def f_3(x):
+    return x**3 + 5 * x
+
+
+def d_f(x):
+    return 2 * x
+
+
+def d_f_2(x):
+    return 3 * x**2
+
+
+def d_f_3(x):
+    return 3 * x**2 + 5
+
+
+# def vector_sum(vx, vy):
+#     x = vx[0]
+#     y = vy[0]
+#     return (x**2 + y**2)**0.5
