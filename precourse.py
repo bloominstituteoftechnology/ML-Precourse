3d9b469c096ba4bfaa3fd74d7201dfec96a50425
astrebeck <alexstrebeck@gmail.com>
2018-03-14 18:34:19 -0600

    added several functions

--- precourse.py
+++ precourse.py
@@ -8,6 +8,26 @@
 
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
+import numpy as np
+
+
+
 def f(x):
     return x**2
+
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return (x**3 + 5*x)
+
+def d_f(x):
+    return 2*x
+
+def d_f_2(x):
+    return 3*x
+
+def d_f_3(x):
+    return (3*x + 5)
+
 
