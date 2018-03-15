3819b580f53fbb16e6397b96dd1a73811301c29b
manukempo <manukempo@gmail.com>
2018-03-15 18:09:06 +0000

    Machine Learning - Homework 3

--- precourse.py
+++ precourse.py
@@ -8,6 +8,49 @@
 
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
+
+# Calculus
 def f(x):
     return x**2
 
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return x**3 + 5*x
+
+def d_f(x):
+    return 2*x
+
+def d_f_2(x):
+    return 3*x**2
+
+def d_f_3(x):
+    return 3*x**2 + 5
+
+# Linear Algebra
+import numpy as np
+
+def vector_sum(a,b):
+    return np.add(a,b)
+
+def vector_less(a,b):
+    return np.subtract(a,b)
+
+def vector_magnitude(v):
+    return np.sqrt(np.dot(v,v))
+
+def vec5():
+    return np.ones(5)
+
+def vec3():
+    return np.zeros(3)
+
+def vec2_1():
+    return np.array([1,0])
+
+def vec2_2():
+    return np.array([0,1])
+
+def matrix_multiply(v, m):
+    return np.dot(v,m)
