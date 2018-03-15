3337aeccfd8f165519f9a18371152ce5945f1435
astrebeck <alexstrebeck@gmail.com>
2018-03-14 19:01:26 -0600

    added several functions

--- precourse.py
+++ precourse.py
@@ -9,6 +9,7 @@
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
 import numpy as np
+import math
 
 
 
@@ -30,4 +31,48 @@ def d_f_2(x):
 def d_f_3(x):
     return (3*x + 5)
 
+def vector_sum(x,y):
+    return np.add(x,y)
 
+def vector_less(x,y):
+    return np.subtract(x,y)
+
+def vector_magnitude(x):
+
+    y=0
+
+    for( number in x):
+
+        y += number**2
+
+    return math.sqrt(y)
+
+def vec5():
+
+    x = np.array(np.ones(5,1))
+
+    return x
+
+def vec3():
+
+    x = np.array(np.zeros(3,1))
+
+    return x
+
+def vec2_1():
+
+    x = np.array([[1],[0]])
+
+    return x
+
+def vec2_2():
+
+    x = np.array([[0],[1]])
+
+    return x
+
+def matrix_multiply():
+
+    x = np.array([[0],[1]])
+
+    return x
