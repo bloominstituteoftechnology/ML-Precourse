b1127bc57e48baa94278a874b2e53947bb6e0429
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-03-16 16:33:16 -0700

    10 functions added

--- precourse.py
+++ precourse.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 # Machine Learning/Data Science Precourse Work
 
 # ###
@@ -9,5 +11,39 @@
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
 def f(x):
-    return x**2
-
+    return x ** 2
+def f_2(x):
+    return x ** 3
+def f_3(x):
+    return x ** 3 + x * 5
+def d_f(x):
+    return x * 2
+def d_f_2(x):
+    return 3 * x ** 2
+def d_f_3(x):
+    return(3 * x ** 2) + 5
+def vector_sum(x, y):
+    output = []
+    for x, y in zip(x, y):
+        output.append(x + y)
+    return output
+def vector_less(x, y):
+    output = []
+    for x, y in zip(x, y):
+        output.append(x - y)
+    return output
+def vector_magnitude(vec):
+    output = 0
+    for i in vec:
+        output += i**2
+    return math.sqrt(output)
+def vec5():
+    return np.array([1, 1, 1, 1, 1])
+def vec3():
+    return np.array([0, 0, 0])
+def vec2_1():
+    return np.array([1, 0])
+def vec2_2():
+    return np.array([0, 1])
+def matrix_multiply(vec, matrix):
+    return np.array(matrix).dot(vec)
