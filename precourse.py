9f011cbc16399b8ec85f4c0263c96b2da61090d4
Brenner Haverlock <doba@rrcs-108-169-151-47.midsouth.biz.rr.com>
2018-03-20 16:08:26 -0400

    fixed my error

--- precourse.py
+++ precourse.py
@@ -7,11 +7,11 @@
 # ###
 
 # Free example function definition
+# This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
 import numpy as m
 import math
-
 def f(x):
-        return x**2
+    return x**2
 
 def f_2(x):
         return x**3
