bfce90c6cac973a46091a07ab95497ea95e5cfdf
Brenner Haverlock <doba@rrcs-108-169-151-47.midsouth.biz.rr.com>
2018-03-20 16:44:25 -0400

    tried again

--- precourse.py
+++ precourse.py
@@ -5,7 +5,7 @@
 # ###
 # MIT LICENSE
 # ###
-
+##
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
 import numpy as m
