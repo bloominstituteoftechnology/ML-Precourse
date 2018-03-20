30adf83b922e90cd1a7e4276bfe7a8c85f66df7c
lmm78 <luke_miller@alumni.brown.edu>
2018-03-19 21:44:01 -0700

    precourse hmwk

--- precourse.py
+++ precourse.py
@@ -1,5 +1,5 @@
 # Machine Learning/Data Science Precourse Work
-
+import numpy as np
 # ###
 # LAMBDA SCHOOL
 # ###
@@ -11,3 +11,44 @@
 def f(x):
     return x**2
 
+def f_2(x):
+    return x**3
+
+def f_3(x):
+    return x**3 + 5*x
+
+def d_f(x):
+    return 2 * x
+
+def d_f_2(x):
+    return 3*x**2
+
+def d_f_3(x):
+    return 3*x**2 + 5
+
+def vector_sum(x,y):
+    return np.add(x,y)
+
+def vector_sum(x,y):
+    return np.add(x,y)
+
+def vector_less(x,y):
+    return np.subtract(x,y)
+
+def vector_magnitude(x):
+    return np.linalg.norm(x)
+
+def vec5():
+    return np.array([1,1,1,1,1])
+
+def vec3():
+    return np.array([0,0,0])
+
+def vec2_1():
+    return np.array([1,0])
+
+def vec2_2():
+    return np.array([0,1])
+
+def matrix_multiply(vec,matrix):
+    return np.dot(vec,matrix)
