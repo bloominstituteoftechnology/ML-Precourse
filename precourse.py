54803360866a1282b2aa917d5198eaabf6ca1487
Joanne Jordan <Joanne.k.m.jordan@gmail.com>
2018-03-27 13:28:54 -0400

    Joanne Jordan precourse.py

--- precourse.py
+++ precourse.py
@@ -10,4 +10,31 @@
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
 def f(x):
     return x**2
+def f_2(x):
+  return x**3
+def f_3(x):
+  return x**3+5*x
+def d_f(x):
+  return 2*x
+def d_f_2(x):
+  return 3*x**2
+def d_f_3(x):
+  return 3*x**2+5
+def vector_sum(x,y):
+  return x+y
+def vector_less(x,y):
+  return x-y
+def vector_magnitude(v):
+  return (sum(i**2 for i in v))**.5
+import numpy as np
+def vec5():
+  return np.array([1,1,1,1,1])
+def vec3():
+  return np.array([0,0,0])
+def vec2_1():
+  return np.array([1,0])
+def vec2_2():
+  return np.array([0,1])
+def matrix_multiply(vec,matrix):
+  return np.array([vec[0]*matrix[0,0]+vec[1]*matrix[0,1],vec[0]*matrix[1,0]+vec[1]*matrix[1,1]])
 
