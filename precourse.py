27a07402481dfdb129b67005266121f4c63fb381
jgowman <34351909+jgowman@users.noreply.github.com>
2018-06-22 14:48:44 -0400

    Update precourse.py

--- precourse.py
+++ precourse.py
@@ -11,3 +11,48 @@
 def f(x):
     return x**2
 
+def f_2(x):
+	return x**3
+
+def f_3(x):
+	return (x**3 + 5*x)
+
+def d_f(x):
+	return 2*x
+
+def d_f_2(x):
+	return 3*x**2
+
+def d_f_3(x):
+	return (3*x**2 + 5)
+
+def vector_sum(x,y):
+	return [x[0]+y[0]]
+
+def vector_less(x,y):
+	return [(x[0]-y[0])]
+
+def vector_magnitude(x):
+	import math
+	return math.sqrt(x[0]**2 + x[1]**2)
+
+def vec5():
+	import numpy as np
+	return np.array([1,1,1,1,1])
+
+def vec3():
+	import numpy as np
+	return np.array([0,0,0])
+
+def vec2_1():
+	import numpy as np
+	return np.array([1,0])
+
+def vec2_2():
+	import numpy as np
+	return np.array([0,1])
+
+def matrix_multiply(vec,matrix):
+	import numpy as np
+	return np.dot(vec,matrix)
+
