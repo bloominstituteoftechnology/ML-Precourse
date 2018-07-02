42468937e8295712abae3b13565f322903180a6d
Oz-Walt <40534858+Oz-Walt@users.noreply.github.com>
2018-07-01 20:58:32 -0500

    Update precourse.py

--- precourse.py
+++ precourse.py
@@ -31,3 +31,24 @@ def d_f_3(x):
     
 def vector_sum(x,y):
     return np.add(x,y)
+
+def vector_less(x,y):
+    return np.subtract(x,y)
+
+def vector_magnitude(x):
+    return np.linalg.norm(x)
+
+def vec5():
+    return np.array (1,1,1,1,1)
+
+def vec3():
+    return np.array (0,0,0)
+
+def vec2_1():
+    return np.array (1,0)
+
+def vec2_2():
+    return np.array (0,1)
+
+def matrix_multiple(vert, matrix);
+    return np.dot(vert, matrix)
