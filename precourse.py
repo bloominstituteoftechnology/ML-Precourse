c5c39bc009eb1d5af8cbeff915998c7d4fa41c29
Oz-Walt <40534858+Oz-Walt@users.noreply.github.com>
2018-07-01 19:26:31 -0500

    Update precourse.py

--- precourse.py
+++ precourse.py
@@ -17,11 +17,11 @@ def f_2(X):
 def f_3(x):
     return x**3 + 5
 
-def d_f(f):
+def d_f(f(x)):
     2*x**1
     
-def d_f_2(f_2):
+def d_f_2(f_2(x)):
     3*x**2
     
-def d_f_3(f_3)
+def d_f_3(f_3(x)):
     3*x**2 +5*x
