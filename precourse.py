275975b3b27e57410c57ef4c23a893616a3d276c
Oz-Walt <40534858+Oz-Walt@users.noreply.github.com>
2018-07-01 20:33:30 -0500

    Update precourse.py

--- precourse.py
+++ precourse.py
@@ -8,6 +8,9 @@
 
 # Free example function definition
 # This function passes one of the 11 tests contained inside of test.py. Write the rest, defined in README.md, here, and execute python test.py to test. Passing this precourse work will greatly increase your odds of acceptance into the program.
+
+import numpy as np
+
 def f(x):
     return x**2
 
